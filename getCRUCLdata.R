56fb2caf6efa77520f9c6a996e909fa9a7965c55
dondealban <dondealban@gmail.com>
2020-03-29 11:48:20 +0800

    Set up basic script, script description and header comment section

---
+++ getCRUCLdata.R
@@ -0,0 +1,14 @@
+# Script Description ----------------------
+# This script implements a basic workflow to extract the CRU CL v.2.0 climate datasets
+# using the getCRUCLdata R package (Sparks 2019). More information about the getCRUCLdata
+# R package found here: https://cran.r-project.org/web/packages/getCRUCLdata/index.html.
+#
+# Script by:      Jose Don T. De Alban
+# Date created:   29 Mar 2020
+
+
+# Set Working Directory -------------------
+
+
+# Load Libraries --------------------------
+# Read Input Data -------------------------
